84fd83a7a569f66305b79b1957a8bb0fa7eb232a
Joonas Loppi <joonas+code@joonas.fi>
2025-02-02 16:31:30 +0200

    individual view/edit of items, only link to that view from TODO items, ...
    - also show recent scans (most recent scans first)
    
    closes #1
    closes #2

--- cmd/shopping-list-manager/webui.go
+++ cmd/shopping-list-manager/webui.go
@@ -10,10 +10,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
 
 	. "github.com/function61/gokit/builtin"
 	"github.com/function61/gokit/net/http/httputils"
 	"github.com/joonas-fi/shopping-list-manager/pkg/todoist"
+	"github.com/samber/lo"
 )
 
 //go:embed *.html
@@ -30,13 +32,12 @@ func webUI(ctx context.Context, todo *todoist.Client, logger *log.Logger) error 
 	}
 
 	routes := http.NewServeMux()
+
 	routes.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/shopping-list-manager/", http.StatusFound)
 	})
-	routes.HandleFunc(appHomeRoute, httputils.WrapWithErrorHandling(func(w http.ResponseWriter, r *http.Request) error {
-		barcode := r.URL.Query().Get("barcode")
-		productName := r.URL.Query().Get("name")
 
+	routes.HandleFunc(appHomeRoute, httputils.WrapWithErrorHandling(func(w http.ResponseWriter, r *http.Request) error {
 		beep := r.URL.Query().Get("beep")
 
 		if beep != "" {
@@ -53,7 +54,44 @@ func webUI(ctx context.Context, todo *todoist.Client, logger *log.Logger) error 
 			return nil
 		}
 
-		if barcode != "" && productName != "" {
+		db, err := loadDB()
+		if err != nil {
+			return err
+		}
+		type productDetailsWrapped struct {
+			productDetails
+			Barcode string
+		}
+		db_ := lo.MapToSlice(*db, func(key string, value productDetails) productDetailsWrapped {
+			return productDetailsWrapped{
+				productDetails: value,
+				Barcode:        key,
+			}
+		})
+
+		sort.Slice(db_, func(i, j int) bool {
+			if db_[i].LastScanned == nil {
+				return false
+			}
+			if db_[j].LastScanned == nil {
+				return true
+			}
+
+			return !db_[i].LastScanned.Before(*db_[j].LastScanned)
+		})
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		return templates.ExecuteTemplate(w, "index.html", db_)
+	}))
+
+	routes.HandleFunc(appHomeRoute+"item/{barcode}", httputils.WrapWithErrorHandling(func(w http.ResponseWriter, r *http.Request) error {
+		barcode := r.PathValue("barcode")
+		db, err := loadDB()
+		if err != nil {
+			return err
+		}
+
+		if productName := r.URL.Query().Get("name"); productName != "" {
 			if err := recordMissAndStoreToLocalDB(r.Context(), barcode, newProductDetails(productName, ""), todo); err != nil {
 				return err
 			}
@@ -62,21 +100,22 @@ func webUI(ctx context.Context, todo *todoist.Client, logger *log.Logger) error 
 			_, err := fmt.Fprintf(w, "added to recognized barcodes: %s", productName)
 			return err
 		}
-		misses, err := listMisses(ctx, todo)
-		if err != nil {
-			return err
+
+		item, found := (*db)[barcode]
+		if !found {
+			item = newProductDetails(taskNameForUnnamedBarcode(barcode), "")
 		}
-
-		missed := func() string {
-			if len(misses) > 0 {
-				return misses[0]
-			} else {
-				return ""
-			}
-		}()
-
+		type itemWrapped struct {
+			productDetails
+			Barcode string // since this is found from DB key only (not present in the actual item)
+			Found   bool
+		}
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		return templates.ExecuteTemplate(w, "index.html", struct{ Barcode string }{missed})
+		return templates.ExecuteTemplate(w, "item.html", itemWrapped{
+			productDetails: item,
+			Found:          found,
+			Barcode:        barcode,
+		})
 	}))
 
 	srv := &http.Server{
